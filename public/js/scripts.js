ca547076e0e2c7e933440d607589c44ac764eac6
LucianoBonfiglio <luciano.bonfiglio88@gmail.com>
2020-05-22 12:22:07 -0300

    Nuevos campos y API de provincias

---
+++ public/js/scripts.js
@@ -0,0 +1,15 @@
+// APi Provincias
+window.addEventListener("load", function() {
+    fetch("https://apis.datos.gob.ar/georef/api/provincias?campos=id,nombre")
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(json) {
+        option = document.getElementById("provincia")
+        tipos = json.provincias
+  
+        for (var i = 0; i < tipos.length; i++) {
+          option.innerHTML += '<option value "' + tipos[i].nombre + '">' + tipos[i].nombre +'</option>'
+        }
+      })
+  });
--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -1,25 +1,50 @@
 @extends('layouts.app')
 @section('content')
+<script type="text/javascript" src={{ asset('js/scripts.js') }}></script>
 	<form>
 	  <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
 	    <label for="nombre">Nombre:</label>
-	    <input type="text" class="form-control" id="nombre">
+	    <input type="text" class="form-control" id="nombre" name="nombre">
 	  </div>
 	  <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
 	    <label for="apellido">Apellido:</label>
-	    <input type="text" class="form-control" id="apelido">
+	    <input type="text" class="form-control" id="apelido" name="apellido">
 	  </div>  
 	  <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
 	    <label for="email">Email:</label>
-	    <input type="email" class="form-control" id="email">
+	    <input type="email" class="form-control" id="email" name="email">
 	  </div>
 	  <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
 	    <label for="dni">DNI:</label>
-	    <input type="text" class="form-control" id="dni">
+	    <input type="text" class="form-control" id="dni" name="dni">
 	  </div>
-	   <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+	  <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+	  <label for="provincia">Provincia:</label>
+	  <select id="provincia" class="form-control" name="provincia">
+	  	<option value="" selected disabled hidden>Seleccione</option>
+    	<option value = ""></option>
+      </select>
+	  </div>
+	  <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+	    <label for="telefono">Telefono:</label>
+	    <input type="integer" class="form-control" id="telefono" name="telefono">
+	  </div>  
+	  <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+	  <label for="sexo">Sexo:</label>
+	  <select id="sexo" class="form-control" name="sexo">
+	  	<option value="" selected disabled hidden>Seleccione</option>
+    	<option value = "masculino">Masculino</option>
+		<option value = "femenino">Femenino</option>
+      </select>
+	  </div>
+	  <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+	    <label for="oficio">Oficio:</label>
+	    <input type="oficio" class="form-control" id="oficio" name="oficio">
+	  </div>
+    
+	  <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
 	    <button type="submit" class="btn btn-default">Submit</button>
-	  </div>    
+	  </div>
 	</form>
 	</div>
 </body>
